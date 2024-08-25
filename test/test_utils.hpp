535a16b7acfeeede168dcda44e6d842430e4b043
Sean McBane <sean.mcban@protonmail.com>
2024-08-24 21:43:32 -0500

    Add a simpler RNG to use for testing.

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -10,49 +10,18 @@
 
 inline uint64_t rotl( uint64_t x, int k ) { return ( x << k ) | ( x >> ( 64 - k ) ); }
 
-struct xoshiro256ss {
-    typedef uint64_t result_type;
-    static constexpr auto min() { return std::numeric_limits< uint64_t >::min(); }
-    static constexpr auto max() { return std::numeric_limits< uint64_t >::max(); }
-
-    xoshiro256ss() {
-        static_assert( sizeof( uint64_t ) == 2 * sizeof( unsigned ) ||
-                       sizeof( uint64_t ) == sizeof( unsigned ) );
-        std::random_device rd;
-        for ( int i = 0; i < 4; ++i ) {
-            m_state[ i ] = rd();
-            if constexpr ( sizeof( uint64_t ) > sizeof( unsigned ) ) {
-                m_state[ i ] += uint64_t( rd() ) << 32;
-            }
-        }
-    }
-
-    void set_state( uint64_t a, uint64_t b, uint64_t c, uint64_t d ) {
-        m_state[ 0 ] = a;
-        m_state[ 1 ] = b;
-        m_state[ 2 ] = c;
-        m_state[ 3 ] = d;
-    }
+class Splitmix64 {
+    explicit Splitmix64( uint64_t state ) : m_state{ state } {}
 
     uint64_t operator()() {
-        uint64_t result = rotl( m_state[ 1 ] * 5, 7 ) * 9;
-        uint64_t t = m_state[ 1 ] << 17;
-
-        m_state[ 2 ] ^= m_state[ 0 ];
-        m_state[ 3 ] ^= m_state[ 1 ];
-        m_state[ 1 ] ^= m_state[ 2 ];
-        m_state[ 0 ] ^= m_state[ 3 ];
-        m_state[ 2 ] ^= t;
-        m_state[ 3 ] = rotl( m_state[ 3 ], 45 );
-
-        return result;
+        uint64_t out = m_state += 0x9E3779B97f4A7C15;
+        out = ( out ^ ( out >> 30 ) ) * 0xBF58476D1CE4E5B9;
+        out = ( out ^ ( out >> 27 ) ) * 0x94D049BB133111EB;
+        return out ^ ( out >> 31 );
     }
 
-    using state_type = uint64_t[ 4 ];
-    const state_type& state() const { return m_state; }
-
   private:
-    uint64_t m_state[ 4 ];
+    uint64_t m_state;
 };
 
 struct NoHash {
